eb542e69048398880a61b82db6f60477b801fb53
Rajabu-Mrisho <rajabumrisho69@gmail.com>
2021-09-16 22:58:02 +0300

    codes

--- iconnect/settings.py
+++ iconnect/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8y&s!81(ig!-(bo#k-f@c9_l(hlo%-e2@=&pqd&m3lpwim^53o'
+SECRET_KEY = 'do9ookmmnu=>RajabuMrisho<=ie-8y&s!81(ig!-(bo#k-f@c9_l(hlo%-e2@=&pqd&m3lpwim^53o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'https://iconnect2.herokuapp.com', 'iconnect2.herokuapp.com']
 
 
 # Application definition
